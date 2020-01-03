16d6e02bd32a6c4209f3e853ee7bf582e8aa55af
Yandry Zambrano <58402156+Yandry-Zambrano@users.noreply.github.com>
2020-01-03 15:12:44 -0500

    principiosfinal

---
+++ carrito-de-compras-master/CarritoDeCompras/Impresora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CarritoDeCompras
+{
+    public class Impresora
+    {
+        public Impresora()
+        {
+        }
+		//se crea una clase solo para imprimir los datos
+        //poniendo en practica el primer principio de responsabilidad unica
+        public void ImprimirDatosCliente(Cliente cliente)
+        {
+            
+            Console.WriteLine(cliente.Apellidos + cliente.Nombres);
+
+            
+            
+        }
+    }
+}
